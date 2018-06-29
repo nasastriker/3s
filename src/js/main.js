38b8d3be59049f8cbb43451faf4995900662c2c8
nasastriker-pc\nasastriker <nasasttriker88@gmail.com>
2018-06-29 13:34:18 +0300

    Fix

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,30 @@
     Custom
  */
 //= partials/helper.js
+
+$('.casesNews-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        }
+    }
+});
+
+$(window).scroll(function () {
+	var movement = -parseInt($(this).scrollTop() / 10);
+	console.log(movement);
+
+	$('body').css({
+		backgroundPosition: 'center ' + movement + 'px'
+	});
+
+	$('.ourAdvertisers').css({
+		backgroundPosition: '50% ' + (movement + 400) + 'px'
+	});
+});
