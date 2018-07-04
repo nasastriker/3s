f37111465af16e943679f2a113014a1b702cb377
nasastriker-pc\nasastriker <nasasttriker88@gmail.com>
2018-07-04 11:51:38 +0300

    Fix

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,13 @@
     Custom
  */
 //= partials/helper.js
-
+$(window).on('load', function () {
+    $preloader = $('.loaderAreaWrapper'),
+        $loader = $preloader.find('.loader');
+        
+    $loader.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
 $('.casesNews-carousel').owlCarousel({
     loop:true,
     margin:10,
